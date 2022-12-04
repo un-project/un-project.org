b913beb3b61e5aec076f91998d9a96de99e422cc
cravesoft <cravesoft@gmail.com>
2022-12-04 15:09:31 +0100

    add dc.js

--- src/app.js
+++ src/app.js
@@ -8,5 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/v1', indexRouter);
+app.use((err, req, res, next) => {
+  res.status(400).json({ error: err.stack });
+});
 
 export default app;
